4d6c1994d0e14790d65882dfb026e7d060f8a660
aqeeladil <aqeel.adil17@gmail.com>
2025-04-10 00:53:28 +0500

    requests and responses

---
+++ 08-official-drf/02-requests-and-responses/snippets/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns     # 
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+'''
+this will add the suffixes to the URLs. for example, if you have a URL like /snippets/1.json, it will return the 
+JSON representation of the snippet with ID 1. If you have a URL like /snippets/1.xml, it will return the 
+XML representation of the snippet with ID 1. This is useful for APIs that need to support multiple formats.
+
+'''
